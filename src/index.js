63d1741d8c222669bdc913fd63020f2eb41d4b1b
Darya Zelenkova <daryazeliankova@yandex.ru>
2019-10-09 22:23:42 +0300

    fix: change multiply function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function multiply(first, second) {
-  // your solution
-}
+    let firstArray=first.split('').reverse()
+    let secondArray=second.split('').reverse()
+    let sumArray=[]
+      for (let i=0; i<firstArray.length; i++){
+        for (let j=0; j<secondArray.length; j++){
+          let ab=firstArray[i]*secondArray[j]
+          if(!sumArray[j+i]){
+            sumArray[j+i]=ab
+          }
+          else{
+            sumArray[j+i]=sumArray[j+i]+ab
+          }
+        }
+      }
+      for (let i=0; i<sumArray.length; i++){
+        let tenth=Math.floor(sumArray[i]/10)
+        if(tenth>0&&i<sumArray.length-1){
+          sumArray[i]=sumArray[i]%10
+          sumArray[i+1]=sumArray[i+1]+tenth
+        }
+       else if(tenth>0){
+         debugger
+         sumArray[i]=sumArray[i]%10
+         sumArray.push(tenth)
+         break
+       }
+      }
+      return sumArray.reverse().join('')
+    }
